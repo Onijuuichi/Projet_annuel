8566900d254f2092da808b587eb254e1d0461e03
Arthur-grd <art.grouard@gmail.com>
2022-02-02 17:11:20 +0100

    Premier test

--- Essai.py
+++ Essai.py
@@ -1,1 +1,18 @@
-print("Bg");
+import unittest
+
+
+def bg():
+    return "bg";
+
+
+
+print("bgggggg")
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(bg(), "bg")  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
