bba56922443d2cedcb9d996bd2d4a8f7a7e65f73
Shanna <sshanna1609@gmail.com>
2022-02-03 16:35:57 +0100

    Script extraction en class

--- premier_script_test_extraction.py
+++ premier_script_test_extraction.py
@@ -13,20 +13,41 @@ import re
 
 ##------------Calcul nombre d'interruption (dose prescrite!=dose reçu) ---------##
 
-def extraction_dose_prescrite(fichier_out, regle):
-   fichier = open(fichier_out, "r")
-   liste_match=[]
-   for ligne in fichier:
-       match=regle.search(ligne)
-       if match:
-           liste_match.append(match.group())
-   fichier.close()
-   return(liste_match);          
+class Interruption:
     
-        
+    def extraction_dose_prescrite(fichier_out, regle):
+        liste_match_prescrite=[]
+        for ligne in fichier_out['NombreFractionsPrescrites']:
+            match=regle.search(ligne)
+            if match:
+                liste_match_prescrite.append(match.group())
+        return(liste_match_prescrite);          
+      
+#--------------------------------------------------------------------------------#
+    
+    def extraction_dose_recu(fichier_out, regle):
+        liste_match_recu=[]
+        for ligne in fichier_out['NombreFractionsDelivrees']:
+            match=regle.search(ligne)
+            if match:
+                liste_match_recu.append(match.group())
+        return(liste_match_recu); 
+     
+    def calcul_nb_interruption(liste_match_prescrite, liste_match_recu):
+        cpt=0
+        for i in liste_match_prescrite:
+            for j in liste_match_recu:
+                if(liste_match_prescrite(i)!=(liste_match_recu(j))):
+                    cpt=cpt+1      
+        return(cpt) 
 
+#--------------------------------------------------------------------------------# 
+     
 ##---------Test------##
 
-
-regle=re.compile(r"^(\d*)", re.IGNORECASE)
-print (extraction_dose_prescrite("out.txt", regle))
+fichier_out = pd.read_csv("/Users/sshan/OneDrive/Documents/COURS M1/PROJET ANNUEL/Projet_annuel/out.txt", sep='\t', index_col=0)
+regle_doses=re.compile(r"^(\d*)", re.IGNORECASE)
+print ("liste du nb de dose prescrite : " , Interruption.extraction_dose_prescrite(fichier_out, regle_doses))
+print("-------------------------------------------------------")
+print ("liste du nb de dose delivrees : ",  Interruption.extraction_dose_recu(fichier_out, regle_doses))
+#print("nombre d'interruption : ", Interruption. calcul_nb_interruption(liste_match_prescrite, liste_match_recu))
