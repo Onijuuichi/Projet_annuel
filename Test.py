8a2bea2496ba298543b9b1a747db9d251d253e53
Shanna <sshanna1609@gmail.com>
2022-02-17 17:39:36 +0100

    test arthur

--- Test.py
+++ Test.py
@@ -1,32 +1,53 @@
-from premier_script_test_extraction import Interruption
+"""
+Projet annuel M1 GPhy 2021/2022
+
+Script de tests unitaires
+
+@author: arthur
+"""
+
+#Import de la classe Interruption contenue dans "script_test.py"
+from script_test import Interruption
+
+##----------------MODULES IMPORTES-----------------##
 
 import sys
 import unittest
 import pandas as pd
 import re
 
+#Permet de récupérer code du script "script_test.py"
 sys.path.append("..")
 
+##----------------NOUVELLE CLASSE------------------##
 
 class TestInterruption(unittest.TestCase):
+    
+    #----Fonction qui définit les éléments utiles pour réaliser les tests----#
     def setUp(self):
-        self.fichier_out = pd.read_csv("/Users/Arthur/Desktop/PA/Projet_annuel/test_out.txt", sep='\t', index_col=0)
+        self.fichier_out = pd.read_csv("/Users/perar/Documents/GitHub/Projet_annuel/test_out.txt", sep='\t', index_col=0)
         self.regle_doses = re.compile(r"^(\d*)", re.IGNORECASE)
 
-    def test_calcul_nb_interruption(self):
-        self.nb = Interruption.calcul_nb_interruption(self.fichier_out, self.regle_doses)
+    #----Fonction de test de "calcul_nb_interruptions"----#
+    def test_calcul_nb_interruptions(self):
+        self.nb = Interruption.calcul_nb_interruptions(self.fichier_out, self.regle_doses)
         self.assertEquals(self.nb, 3)
 
-    def test_extraction_dose_prescrite(self):
+    #----Fonction de test de "extraction_doses_prescrites"----#
+    def test_extraction_doses_prescrites(self):
         liste_test = ['12', '10', '10', '33', '30', '10', '10', '30', '10', '28', '10', '10', '33', '10', '10', '11',
                       '15', '3', '7', '5', '15', '11'];
-        self.liste = Interruption.extraction_dose_prescrite(self.fichier_out, self.regle_doses)
+        self.liste = Interruption.extraction_doses_prescrites(self.fichier_out, self.regle_doses)
         self.assertListEqual(liste_test,self.liste)
 
-    def test_extraction_dose_recu(self):
+    #----Fonction de test de "extraction_doses_recues"----#
+    def test_extraction_doses_recues(self):
         liste_test = ['12','10','10','32','30','10','9','30','10','28','8','10','33','10','10','11','15','3','7','5','15','11']
-        self.liste = Interruption.extraction_dose_recu(self.fichier_out, self.regle_doses)
+        self.liste = Interruption.extraction_doses_recues(self.fichier_out, self.regle_doses)
         self.assertListEqual(liste_test,self.liste)
 
+##----------------FIN DE LA CLASSE TestInterruption------------------##
+
+#Lancement du test : fournit une interface de ligne de commande au script de test ?
 if __name__ == '__main__':
     unittest.main()
