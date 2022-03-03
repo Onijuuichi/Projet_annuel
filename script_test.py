90a6b0996c8d6b030be7202fe86510a12cb77847
Arthur-grd <art.grouard@gmail.com>
2022-03-03 17:23:31 +0100

    Utilisation de json

--- script_test.py
+++ script_test.py
@@ -13,47 +13,47 @@ import pandas as pd
 import re
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import json
 
 class Interruption:
 
     ##----------------Constructeur---------------------##
 
-    def __init__(self, fichier_out):
-        self.fichier_out = fichier_out;
+    def __init__(self, data):
+        self.data = data;
 
     ##------------Créer la liste des doses prescrites aux patients ---------##
 
-    def extraction_dose_prescrite(fichier_out, regle):
+    def extraction_dose_prescrite(data, regle):
         liste_match_prescrite = []
-        for ligne in fichier_out['NombreFractionsPrescrites']:
-            match = regle.search(ligne)
+        for ligne in data:
+            match = regle.search(ligne['NombreFractionsPrescrites'])
             if match:
                 liste_match_prescrite.append(match.group())
         return (liste_match_prescrite)
 
     # -----------------Créer la liste des doses recu des patients---------------------#
 
-    def extraction_dose_recu(fichier_out, regle):
+    def extraction_dose_recu(data, regle):
         liste_match_recu = []
-        for ligne in fichier_out['NombreFractionsDelivrees']:
-            match = regle.search(ligne)
+        for ligne in data:
+            match = regle.search(ligne['NombreFractionsDelivrees'])
             if match:
                 liste_match_recu.append(match.group())
         return (liste_match_recu)
 
     # -----------------Verifie les noms de traitement (si PTV55 ou PTV27 return(False), sinon return(True)) ---------------------#
 
-    def verifie_nom(fichier_out):
+    def verifie_nom(data):
         liste_verifie_nom = []
-        for ligne in fichier_out['Nom']:
-            if "PTV55" in ligne:
+        for ligne in data:
+            if "PTV55" in ligne['Nom']:
                 valide = False
-            elif "PTV 55" in ligne:
+            elif "PTV 55" in ligne['Nom']:
                 valide = False
-            elif "PTV27" in ligne:
+            elif "PTV27" in ligne['Nom']:
                 valide = False
-            elif "PTV 27" in ligne:
+            elif "PTV 27" in ligne['Nom']:
                 valide = False
             else:
                 valide = True
@@ -62,30 +62,30 @@ class Interruption:
 
     # -----------------Créer la liste des dates de début de traitement (DatePremiereFraction) ---------------------#
 
-    def extraction_date_debut(fichier_out, regle):
+    def extraction_date_debut(data, regle):
         liste_match_date_debut = []
-        for ligne in fichier_out['DatePremiereFraction']:
-            match = regle.search(ligne)
+        for ligne in data:
+            match = regle.search(ligne['DatePremiereFraction'])
             if match:
                 liste_match_date_debut.append(match.group())
         return (liste_match_date_debut)
 
     # -----------------Créer la liste des dates de fin de traitement (DateDerniereFraction) ---------------------#
 
-    def extraction_date_fin(fichier_out, regle):
+    def extraction_date_fin(data, regle):
         liste_match_date_fin = []
-        for ligne in fichier_out['DateDerniereFraction']:
-            match = regle.search(ligne)
+        for ligne in data:
+            match = regle.search(ligne['DateDerniereFraction'])
             if match:
                 liste_match_date_fin.append(match.group())
         return (liste_match_date_fin)
 
     # --------------Verifie si le temps de traitement est supérieur à 4 semaines (si oui return(True), sinon return(False)-------------------#
 
-    def verifie_temps(fichier_out, regle):
+    def verifie_temps(data, regle):
         liste_verifie_temps = []
-        liste_match_date_fin = Interruption.extraction_date_fin(fichier_out, regle)
-        liste_match_date_debut = Interruption.extraction_date_debut(fichier_out, regle)
+        liste_match_date_fin = Interruption.extraction_date_fin(data, regle)
+        liste_match_date_debut = Interruption.extraction_date_debut(data, regle)
         for i in range(len(liste_match_date_fin)):
             time_1 = datetime.strptime(liste_match_date_debut[i], "%d/%m/%Y")
             time_2 = datetime.strptime(liste_match_date_fin[i], "%d/%m/%Y")
@@ -102,12 +102,12 @@ class Interruption:
 
     # --------------Calcul nombre d'interruption (dose prescrite!=dose reçu)-------------------#
 
-    def calcul_nb_interruption(fichier_out, regle1, regle2):
+    def calcul_nb_interruption(data, regle1, regle2):
         interruption = 0
-        liste_verifie_temps = Interruption.verifie_temps(fichier_out, regle2)
-        liste_verifie_nom = Interruption.verifie_nom(fichier_out)
-        liste_match_prescrite = Interruption.extraction_dose_prescrite(fichier_out, regle1)
-        liste_match_recu = Interruption.extraction_dose_recu(fichier_out, regle1)
+        liste_verifie_temps = Interruption.verifie_temps(data, regle2)
+        liste_verifie_nom = Interruption.verifie_nom(data)
+        liste_match_prescrite = Interruption.extraction_dose_prescrite(data, regle1)
+        liste_match_recu = Interruption.extraction_dose_recu(data, regle1)
         for i in range(len(liste_match_prescrite)):
             if (liste_verifie_temps[i] and liste_verifie_nom[i]):
                 if (liste_match_prescrite[i] != liste_match_recu[i]):
@@ -116,23 +116,23 @@ class Interruption:
 
     # ----------------Créer une liste des annees de dernière prescription------------#
 
-    def extraction_annee(fichier_out, regle):
+    def extraction_annee(data, regle):
         liste_match_annee = []
-        for ligne in fichier_out['DateDerniereFraction']:
-            match = regle.search(ligne)
+        for ligne in data:
+            match = regle.search(ligne['DateDerniereFraction'])
             if match:
                 liste_match_annee.append(match.group())
         return (liste_match_annee)
 
     # --------------Calcul nombre d'interruption selon l'annéee-------------------#
 
-    def calcul_nb_interruption_par_an(fichier_out, regle1, regle2, regle3, annee):
+    def calcul_nb_interruption_par_an(data, regle1, regle2, regle3, annee):
         interruption = 0
-        liste_verifie_temps = Interruption.verifie_temps(fichier_out, regle3)
-        liste_verifie_nom = Interruption.verifie_nom(fichier_out)
-        liste_match_prescrite = Interruption.extraction_dose_prescrite(fichier_out, regle1)
-        liste_match_recu = Interruption.extraction_dose_recu(fichier_out, regle1)
-        liste_match_annee = Interruption.extraction_annee(fichier_out, regle2)
+        liste_verifie_temps = Interruption.verifie_temps(data, regle3)
+        liste_verifie_nom = Interruption.verifie_nom(data)
+        liste_match_prescrite = Interruption.extraction_dose_prescrite(data, regle1)
+        liste_match_recu = Interruption.extraction_dose_recu(data, regle1)
+        liste_match_annee = Interruption.extraction_annee(data, regle2)
         for i in range(len(liste_match_prescrite)):
             if (liste_verifie_temps[i] and liste_verifie_nom[i]):
                 if (liste_match_prescrite[i] != liste_match_recu[i] and int(liste_match_annee[i]) == annee):
@@ -141,13 +141,13 @@ class Interruption:
 
     # --------------Calcul final et affichage des résultats par an-------------------#
 
-    def calcul(fichier_out, regle1, regle2, regle3):
+    def calcul(data, regle1, regle2, regle3):
         interruption_liste = []
         print("Nombre d'interruption :")
-        tot_interruption = Interruption.calcul_nb_interruption(fichier_out, regle1, regle3)
+        tot_interruption = Interruption.calcul_nb_interruption(data, regle1, regle3)
         if (tot_interruption > 0):
             for i in range(1955, 2050):
-                interruption = Interruption.calcul_nb_interruption_par_an(fichier_out, regle1, regle2, regle3, i)
+                interruption = Interruption.calcul_nb_interruption_par_an(data, regle1, regle2, regle3, i)
                 if (interruption > 0):
                     interruption_liste.append(interruption)
                     pourcent = (interruption / tot_interruption * 100)
@@ -156,11 +156,16 @@ class Interruption:
             # df = pd.DataFrame(interruption_liste, columns = ['interruption'], index=['20' + str(i) for i in range(14,22)])
             color = sns.color_palette('pastel')
             fig = plt.figure(1, figsize=(10, 10))
-            plt.pie(interruption_liste, labels=['20' + str(i) for i in range(14, 22)], colors=color, autopct="%.1f%%")
+            #plt.pie(interruption_liste, labels=['20' + str(i) for i in range(14, 22)], colors=color, autopct="%.1f%%")
+            plt.plot(['20' + str(i) for i in range(14, 22)], interruption_liste, label="Centre Oscar Lambret")
+            plt.xlabel(' Années')
+            plt.ylabel(' Nombre d\'interrution')
+            plt.title('Nombre d\'interruption de traitement par année')
+            plt.legend("Centre Oscar Lambret")
             plt.legend(interruption_liste)
-            plt.title('Pourcentage d\'interruption de traitement par année')
+            plt.title('Nombre d\'interruption de traitement par année')
             # plt.savefig('/Users/sshan/OneDrive/Documents/COURS M1/PROJET ANNUEL/Projet_annuel/Camembert_interruption.png')
-            plt.savefig('/Users/damienarrive/Documents/GitHub/Projet_annuel/Camembert_interruption.png')
+            plt.savefig('Camembert_interruption.png')
             plt.show()
 
         else:
@@ -169,8 +174,11 @@ class Interruption:
 
 ##---------Test-----------##
 
+test = open('test.json', "r")
+data = json.loads(test.read())
+
 # fichier_out = pd.read_csv("/Users/sshan/OneDrive/Documents/COURS M1/PROJET ANNUEL/Projet_annuel/out.txt", sep='\t', index_col=0, low_memory=(False))
-fichier_out = pd.read_csv("/Users/damienarrive/Documents/GitHub/Projet_annuel/out.txt", sep='\t', index_col=0)
+fichier_out = pd.read_csv("out.txt", sep='\t', index_col=0, low_memory=(False))
 
 ##-------Nos regex-------##
 regle_doses = re.compile(r"^(\d*)",
@@ -184,7 +192,7 @@ regle_date = re.compile(r"\d{2}/\d{2}/\d{4}")  # regex permettant de récupérer
 # print("nombre d'interruption : ", Interruption.calcul_nb_interruption(fichier_out, regle_doses, regle_date))
 # print("Années : ", Interruption.extraction_annee(fichier_out, regle_annee))
 # print("2020 : ", Interruption.calcul_nb_interruption_par_an(fichier_out, regle_doses, regle_annee, regle_date, 2020))
-Interruption.calcul(fichier_out, regle_doses, regle_annee, regle_date)
+Interruption.calcul(data, regle_doses, regle_annee, regle_date)
 # print(Interruption.verifie_nom(fichier_out))
 # print(Interruption.verifie_temps(fichier_out, regle_date))
 
